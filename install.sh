8eeb7f26970b4c3fd39b7b132874852d90647307
TakuyaNakazawa <ntak1113@gmail.com>
2022-06-11 21:01:01 +0900

    change colorscheme to hybrid

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ echo "Create dotfile links."
 ln -snfv ~/dotfiles/.vimrc ~
 ln -snfv ~/dotfiles/vim/settings ~/.vim/settings
 ln -snfv ~/dotfiles/vim/UltiSnips ~/.vim/UltiSnips
+ln -snfv ~/dotfiles/vim/colors ~/.vim/colors
 echo "Success"
 
