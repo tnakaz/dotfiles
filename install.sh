3a7f16797e1b44d06f4e5fa4b980eceefd1aad96
TakuyaNakazawa <ntak1113@gmail.com>
2022-06-15 20:37:31 +0900

    add lazygit config

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ ln -snfv ~/dotfiles/.vimrc ~
 ln -snfv ~/dotfiles/vim/settings ~/.vim/settings
 ln -snfv ~/dotfiles/vim/UltiSnips ~/.vim/UltiSnips
 ln -snfv ~/dotfiles/vim/colors ~/.vim/colors
+rm ~/Library/Application\ Support/lazygit/config.yml
+ln -snfv ~/dotfiles/vim/lazygit/config.yml ~/Library/Application\ Support/lazygit/
 echo "Success"
 
