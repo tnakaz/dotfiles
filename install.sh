637c4e40de0c0aa6ef3506572931c21095edb02e
TakuyaNakazawa <ntak1113@gmail.com>
2022-09-09 08:44:48 +0900

    restrict

--- install.sh
+++ install.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
 echo "Create dotfile links."
-ln -snfv ~/dotfiles/nvim/init.lua ~/.config/nvim
-ln -snfv ~/dotfiles/nvim/lua ~/.config/nvim/lua
-# old
-# ln -snfv ~/dotfiles/.vimrc ~
-# ln -snfv ~/dotfiles/vim/settings ~/.vim/settings
-# ln -snfv ~/dotfiles/vim/UltiSnips ~/.vim/UltiSnips
-# ln -snfv ~/dotfiles/vim/colors ~/.vim/colors
-rm ~/Library/Application\ Support/lazygit/config.yml
-ln -snfv ~/dotfiles/vim/lazygit/config.yml ~/Library/Application\ Support/lazygit/
+brew install stow
+stow -v nvim lazygit tig
+
 # InstallFont
 cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
-ln -snfv ~/dotfiles/tig/.tigrc ~/.tigrc
 echo "Success"
 
